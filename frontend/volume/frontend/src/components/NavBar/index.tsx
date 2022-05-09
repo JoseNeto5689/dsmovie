0a50f41afa158bd388e66512c0c523554912f21b
JoseNeto5689 <jose.sicupiraneto@gmail.com>
2022-05-09 15:28:48 -0300

    Routes

---
+++ frontend/volume/frontend/src/components/NavBar/index.tsx
@@ -0,0 +1,19 @@
+import {ReactComponent as GithubIcon} from "../../assets/img/github.svg"
+import './style.css'
+function NavBar(){
+    return <header>
+      <nav className="container" >
+        <div className="dsmovie-nav-content" >
+          <h1>DSMOVIE</h1>
+          <a href="https://github.com/JoseNeto5689/">
+            <div className="dsmovie-contact-container" >
+              <GithubIcon/>
+              <p className="dsmovie-contact-link" >/JoseNeto5689</p>
+            </div>
+          </a>
+        </div>
+      </nav>
+    </header>
+}
+
+export default NavBar;
